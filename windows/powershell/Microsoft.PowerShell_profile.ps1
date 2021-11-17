3836fc3ba9e391c8752e886ae1a19c79e7a83a01
A1rPun <thombakker@gmail.com>
2021-11-18 00:10:55 +0100

    :pencil: Powershell

--- windows/powershell/Microsoft.PowerShell_profile.ps1
+++ windows/powershell/Microsoft.PowerShell_profile.ps1
@@ -1,4 +1,5 @@
 # Linux:   ~/.config/powershell/Microsoft.PowerShell_profile.ps1
+# Windows: %USERPROFILE%\Documents\PowerShell\Microsoft.PowerShell_profile.ps1
 # Windows: %USERPROFILE%\Documents\WindowsPowerShell\Microsoft.PowerShell_profile.ps1
 
 function prompt {
@@ -11,19 +12,57 @@ function r41nb0w {
   $UserName = [Environment]::UserName
 
   try {
-    $IsAdmin = (New-Object Security.Principal.WindowsPrincipal ([Security.Principal.WindowsIdentity]::GetCurrent())).IsInRole([Security.Principal.WindowsBuiltinRole]::Administrator)
+    $currentUser = New-Object Security.Principal.WindowsPrincipal ([Security.Principal.WindowsIdentity]::GetCurrent())
+    $IsAdmin = $currentUser.IsInRole([Security.Principal.WindowsBuiltinRole]::Administrator)
   }
   catch {
     $IsAdmin = ""
   }
 
-  Write-Host "┌─[" -ForegroundColor Red -NoNewline
+  Write-Host "/[" -ForegroundColor Red -NoNewline
   Write-Host "$MachineName" -ForegroundColor DarkYellow -NoNewline
-  Write-Host "]─[" -ForegroundColor Yellow -NoNewline
+  Write-Host "][" -ForegroundColor Yellow -NoNewline
   Write-Host "$UserName" -ForegroundColor Green -NoNewline
-  Write-Host "]─[" -ForegroundColor Cyan -NoNewline
+  Write-Host "][" -ForegroundColor Cyan -NoNewline
   Write-Host "$pwd" -ForegroundColor Blue -NoNewline
   Write-Host "]" -ForegroundColor Magenta
-  Write-Host "└»" -ForegroundColor Red -NoNewline
-  Write-Host "$IsAdmin" -ForegroundColor Red -NoNewline
+  Write-Host "\" -ForegroundColor Red -NoNewline
+
+  if ($IsAdmin) {
+    Write-Host "Elevated" -ForegroundColor Red -NoNewline
+  }
+  Write-Host ">" -ForegroundColor Red -NoNewline
 }
+
+function notepad++($file) {
+  $programs = [Environment]::GetEnvironmentVariable('programfiles')
+  try {
+    & "$programs\Notepad++\notepad++.exe" $file
+  } catch {
+    notepad++Fallback($programs, $file)
+  }
+}
+
+function notepad++Fallback($programs, $file) {
+  try {
+    & "$programs (x86)\Notepad++\notepad++.exe" $file
+  } catch {}
+}
+
+function history_all {
+  Get-Content (Get-PSReadLineOption).HistorySavePath
+}
+
+function mklink ($link, $target) {
+  New-Item -Path $link -ItemType SymbolicLink -Value $target
+}
+
+function .. {
+  cd ..
+}
+
+Set-Alias touch New-Item
+Set-Alias grep Select-String
+Set-Alias nano notepad++
+Set-Alias vim nano
+Set-Alias vi vim
