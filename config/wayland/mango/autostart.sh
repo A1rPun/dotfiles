d030ed737174705072eca2b8f7e711e4f3e658b7
A1rPun <thombakker@gmail.com>
2026-07-30 13:39:46 +0200

    :mango: mango config

--- config/wayland/mango/autostart.sh
+++ config/wayland/mango/autostart.sh
@@ -13,11 +13,7 @@ swaybg -i ~/Media/wallpaper.jpg >/dev/null 2>&1 &
 waybar >/dev/null 2>&1 &
 
 # keep clipboard content
-# wl-clip-persist --clipboard regular --reconnect-tries 0 >/dev/null 2>&1 &
-exec wl-paste -t text --watch clipman store >/dev/null 2>&1 &
-
-# clipboard content manager
-wl-paste --type text --watch cliphist store >/dev/null 2>&1 &
+wl-paste --type text --watch clipman store >/dev/null 2>&1 &
 
 # Notification demon
 mako >/dev/null 2>&1 &
